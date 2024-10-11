ba17e6901561757ff9cd6ffee6640e54e2948554
t20kdc <asdd2808@gmail.com>
2024-10-11 14:45:10 +0100

    Finish cherry-picking to be 0.29.1-equivalent protocol-wise

--- SS14.Launcher/LauncherVersion.cs
+++ SS14.Launcher/LauncherVersion.cs
@@ -5,5 +5,5 @@ namespace SS14.Launcher;
 public static class LauncherVersion
 {
     public const string Name = "SS14.Launcher";
-    public static Version? Version => new Version("0.29.0");
+    public static Version? Version => new Version("0.29.1");
 }
