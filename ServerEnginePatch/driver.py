6dc6826a978500aff8b8f06161632cb4ae481169
t20kdc <asdd2808@gmail.com>
2024-10-11 20:28:38 +0100

    ServerEnginePatch: Now covers basically all engine versions released this year.

--- ServerEnginePatch/driver.py
+++ ServerEnginePatch/driver.py
@@ -20,39 +20,52 @@ patches = json.load(open("patches.json", "r"))
 tags_unfixed = open("tags", "r").readlines()
 tags = []
 for tag in tags_unfixed:
+	if tag.strip().endswith("-pvsdebug"):
+		continue
 	tags.append(tag.strip())
 tags.sort(key=versionkey)
 
 def check(args):
+	print(args)
 	res = subprocess.run(args)
 	if res.returncode != 0:
 		raise Exception("Invalid")
+
+def get_current_commit():
+	return open("current.commit", "r").readline().strip()
+
+def set_current_commit(value):
+	commit_file = open("current.commit", "w")
+	commit_file.write(value)
+	commit_file.close()
 
 print("setting up patches...")
 
 for version in patches:
 	patch = patches[version]
-	print(" " + patch["patch"])
-	check(["./setup-patch", patch["base"], patch["patch"]])
-	ref = open(patch["patch"] + ".commit", "r").readline().strip()
+	if patch is None:
+		continue
+	print(" " + version)
+	ref = patch["base"]
+	for v in patch["list"]:
+		check(["./setup-patch", ref, v])
+		ref = get_current_commit()
 	print(" " + ref)
 	# This is something that "git cherry-pick" can use.
 	# Git is better at compensating for changes, since it sees the common base.
-	patch["ref"] = ref
+	patch["range"] = patch["base"] + ".." + ref
 
 print("applying to tags...")
 
-def set_patch_commit(patch):
-	commit_file = open("current.commit", "w")
-	commit_file.write(patch["ref"])
-	commit_file.close()
-
-active = False
+patch_range = None
 
 for tag in tags:
 	if tag in patches:
-		active = True
-		set_patch_commit(patches[tag])
-	if active:
+		if patches[tag] is None:
+			patch_range = None
+		else:
+			patch_range = patches[tag]["range"]
+	if not (patch_range is None):
 		print(tag)
-		check(["./cross-pick", tag])
+		check(["./cross-pick", tag, patch_range, "newkey-auto-" + tag])
+		patch_range = tag + ".." + get_current_commit()
